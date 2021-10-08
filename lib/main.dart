88eeabb4dd55aabc08a48f9f3743b1555c95683e
Samuel <zamuelfernandes@gmail.com>
2021-10-08 10:12:18 -0300

    Trabalhando com futuro e iniciando layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,41 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
-import 'package:async/async.dart';
+import 'dart:async';
 import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=ba604901";
 
 void main() async {
-  http.Response response = await http.get(request);
-  json.decode(response.body)["results"]["currencies"];
-
   runApp(MaterialApp(
-    home: Container(),
+    home: Home(),
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
+}
+
+class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text("\$ Conversosr \$"),
+        backgroundColor: Colors.amber,
+        centerTitle: true,
+      ),
+    );
+  }
 }
