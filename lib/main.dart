d6e78a93bc9a5052a4033cdb95f2277788294154
Samuel <zamuelfernandes@gmail.com>
2021-10-08 09:55:15 -0300

    Ajustando data request

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,14 @@ import 'package:flutter/material.dart';
 
 import 'package:http/http.dart' as http;
 import 'package:async/async.dart';
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json&key=ba604901";
 
-void main() async{
+void main() async {
   http.Response response = await http.get(request);
+  json.decode(response.body)["results"]["currencies"];
+
   runApp(MaterialApp(
     home: Container(),
   ));
